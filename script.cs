b6db10ec07937da356fa547847b59c2c5538c361
Y0Ushi <113269922+Y0Ushi@users.noreply.github.com>
2022-09-11 07:36:37 +0600

    Create script.cs

---
+++ script.cs
@@ -0,0 +1,23 @@
+ublic Transform Target; // Drag the object that will be followed in the inspector.
+public Transform Camera; // Drag the camera object in the inspector.
+Vector3 tempVec3 = new Vector3(); // Temporary vector 3.
+
+void LateUpdate()
+{
+  	// If the target is active in the scene, set the x camera position to target.
+    if (Target != null)
+    {
+        tempVec3.x = Target.position.x;
+        tempVec3.y = this.transform.position.y;
+        tempVec3.z = this.transform.position.z;
+        this.transform.position = tempVec3;
+    }
+  	// If target is not active in the scene, set the x camera position to itself.
+    else if (Target == null)
+    {
+        tempVec3.x = Camera.position.x;
+        tempVec3.y = Camera.transform.position.y;
+        tempVec3.z = Camera.transform.position.z;
+        Camera.transform.position = tempVec3;
+    }
+}
